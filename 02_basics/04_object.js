36213130f489f075c90f134919b1592602680434
niharvaishnani <167516198+niharvaishnani@users.noreply.github.com>
2025-07-30 05:19:26 +0000

    object2

---
+++ 02_basics/04_object.js
@@ -0,0 +1,24 @@
+// singletaned   
+
+// const person=new Object(); // creating an empty object using the Object constructor
+
+const person={
+    emaill: "nihar@chatgpt.com",
+    fullname:{
+        firstname: "Nihar",
+        lastname: "Patel"
+    }
+} 
+
+console.log(person.fullname.firstname) // accessing nested object property;
+console.log(person["fullname"]["lastname"]) // accessing nested object property using bracket notation  
+
+const obj1={1: "one", 2: "two"}; // object with numeric keys
+const obj2={3: "uno", 4: "dos"}; // another object 
+
+// using Object.assign to merge objects
+// const mergedObj=Object.assign({}, obj1, obj2); // merging obj1 and obj2 into a new object
+// console.log(mergedObj);   
+
+const mergedObj={...obj1, ...obj2}; // using spread operator to merge objects
+console.log(mergedObj); // { '1': 'one', '2': 'two', '3': 'uno', '4': 'dos' }
