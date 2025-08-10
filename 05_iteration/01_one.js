719dbb06725277bdd7e7f08aa3e3bc42ca55926e
niharvaishnani <167516198+niharvaishnani@users.noreply.github.com>
2025-08-10 04:34:10 +0000

    loops

---
+++ 05_iteration/01_one.js
@@ -0,0 +1,38 @@
+// for loop  
+
+// for (let i = 0; i <= 5; i++) { 
+//     const element = i;
+//     if (i === 3) {
+//         console.log("Skipping number 3");
+//        continue; // skips the rest of the loop for this iteration
+//     }
+//     console.log( i);
+// }  
+
+//let myarry= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+
+// for (let i=0; i<myarry.length; i++) {
+//     const element = myarry[i]; 
+//    // console.log(element); 
+// }
+     
+// while and do while loop  
+
+// let i = 0;
+// while(i<10)
+// {
+//     console.log(i);
+//     i=i+2
+    
+// }         
+
+let arry= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+
+let a=0;
+
+while(a<arry.length ) {       
+    //console.log(arry[a]);  
+    a++; // incrementing a to avoid infinite loop
+}  
+
+    
